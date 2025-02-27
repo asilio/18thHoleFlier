15eced12778e36570c5a597930f65efacb8a8cd0
cwpersonrennell <cpersonrennell@gmail.com>
2025-02-27 11:30:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ for(let i = 0;i<10;i++){
 ComponentFactory.createComponent("Sprite",player.id,"./Assets/player.png");
 ComponentFactory.createComponent("Position",player.id,50,50);
 
-
+let count =0;
 function main(){
+	count++;
 	for(let id in Sprite.Sprites){
 		Sprite.Sprites[id].update(context);
 	}
-	requestAnimationFrame(main);
+	if(count<10000)
+		requestAnimationFrame(main);
 
 }
 
