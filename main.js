c67ad3200d900886bb8d53e1b18ffba4d141b83c
cwpersonrennell <cpersonrennell@gmail.com>
2025-02-27 12:26:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let player= new Entity("Player");
 for(let i = 0;i<10;i++){
 	let t = new Entity("Generic Tile");
 	ComponentFactory.createComponent("Sprite",t.id,"./Assets/tile.png");
-	ComponentFactory.createComponent("Position",t.id,i%5,(i%5)+i);
+	ComponentFactory.createComponent("Position",t.id,32*(i%5),32*Math.floor(i/5));
 }
 ComponentFactory.createComponent("Sprite",player.id,"./Assets/player.png");
 ComponentFactory.createComponent("Position",player.id,50,50);
@@ -15,7 +15,7 @@ let count =0;
 function main(){
 	count++;
 	context.clearRect(0,0,context.canvas.width,context.canvas.height);
-	
+
 	let p = Position.Positions[player.id];
 	p.x+=Math.sin(Date.now()/1000)
 	for(let id in Sprite.Sprites){
