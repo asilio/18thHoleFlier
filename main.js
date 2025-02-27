3c43e702d24f90226841d155cf7495f3a8e7c5a7
cwpersonrennell <cpersonrennell@gmail.com>
2025-02-27 11:14:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const context = document.getElementById("canvas").getContext('2d');
 let player= new Entity("Player");
 for(let i = 0;i<100;i++){
 	let t = new Entity("Generic Tile");
-	ComponentFactory.createComponent("Sprite",t.id,"../Assets/tile.png");
+	ComponentFactory.createComponent("Sprite",t.id,"./Assets/tile.png");
 	ComponentFactory.createComponent("Position",t.id,i%5,(i%5)+i);
 }
-ComponentFactory.createComponent("Sprite",player.id,"../Assets/player.png");
+ComponentFactory.createComponent("Sprite",player.id,"./Assets/player.png");
 ComponentFactory.createComponent("Position",player.id,50,50);
 
 
