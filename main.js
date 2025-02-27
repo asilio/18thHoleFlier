6df50c35986757adf1c8b2a1632ce897341197a0
cwpersonrennell <cpersonrennell@gmail.com>
2025-02-27 12:38:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 import {Entity,Sprite, Position, ComponentFactory} from "./modules/framework.js";
 
+
+/*** Level Data ***/
 const context = document.getElementById("canvas").getContext('2d');
+const TILE_SIZE = 32;
+const WIDTH = 10;
+const HEIGHT = 10;
+
+function pixel_to_tile(px,py){
+	return [px%TILE_SIZE, py%TILE_SIZE];
+}
+
+function tile_to_pixel(tx,ty){
+	return [TILE_SIZE*tx,TILE_SIZE*ty];
+}
+
 
 let player= new Entity("Player");
-for(let i = 0;i<10;i++){
+for(let i = 0;i<WIDTH*HEIGHT;i++){
 	let t = new Entity("Generic Tile");
 	ComponentFactory.createComponent("Sprite",t.id,"./Assets/tile.png");
-	ComponentFactory.createComponent("Position",t.id,32*(i%5),32*Math.floor(i/5));
+	ComponentFactory.createComponent("Position",t.id,TILE_SIZE*(i%WIDTH),TILE_SIZE*Math.floor(i/5));
 }
 ComponentFactory.createComponent("Sprite",player.id,"./Assets/player.png");
 ComponentFactory.createComponent("Position",player.id,50,50);
