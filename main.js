410add83e12848c5d6cb17223313773bac10325f
cwpersonrennell <cpersonrennell@gmail.com>
2025-02-28 15:57:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -301,7 +301,12 @@ function update(time_slice){
 	switch(s){
 		case 'OOB':
 		case 'tree':
-			[playerp.x, playerp.y] = canvas_pixel_to_tile_corner(lastx,lasty);
+			let dx= lastx-playerp.x;
+			let dy = lasty-playerp.y;
+			let d = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
+			dx = dx/d*32;
+			dy = dy/d*32
+			[playerp.x, playerp.y] = canvas_pixel_to_tile_corner(lastx+dx,lasty+dy);
 			M = N+1
 			break;
 		case 'fair':
