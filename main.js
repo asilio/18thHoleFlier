97820ea60df5c3743d6291fba8064a5e50c76a53
cwpersonrennell <cpersonrennell@gmail.com>
2025-02-28 14:28:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -95,8 +95,8 @@ function checkHandedness(){
 
 function checkForeHand(){
 	let fs = Selectable.Selectables[forehand.id];
-	let string = 'forehand';
-	if(fs.isSelected) string = 'backhand';
+	let string = 'backhand';
+	if(fs.isSelected) string = 'forehand';
 	return string;
 }
 
