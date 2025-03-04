8e9a4f853e9ac3fc77654a5de61d829ba886b546
cwpersonrennell <cpersonrennell@gmail.com>
2025-03-04 10:10:24 -0700

    whoopsy

--- featuring-testing.js
+++ featuring-testing.js
@@ -1,62 +1,16 @@
-function LevelOne(){
-	let EID = -1;
-	
-	function Tag(){
-		let id = ++EID;
-		let args = Array.from(arguments);
-		let stride = args.length;
-		if(Tag.components == undefined){
-			Tag.components = [];
-		}
-		for(let i = 0; i<stride;i++){
-			Tag.components[id*stride + i] = args[i];
-		}
-	}
-	
+function RenderIsocubes(el){
+	console.log("Isocube Loaded");
+	console.log(el);
+	console.log(this);
 
-	function Position(){
-		let id = ++EID;
-		let args = Array.from(arguments);
-		let stride = args.length;
-		if(Position.components == undefined){
-			Position.components = [];
-		}
-		for(let i = 0; i<stride;i++){
-			Position.components[id*stride + i] = args[i];
-		}
-	}
-
-
-	function PositionSprite(){
-		let id = ++EID;
-		let args = Array.from(arguments);
-		let stride = args.length;
-		if(PositionSprite.components == undefined){
-			PositionSprite.components = [];
-		}
-		for(let i = 0; i<stride;i++){
-			PositionSprite.components[id*stride + i] = args[i];
-		}
-	}
-
-	function TagPositionSprite(){
-		let id = ++EID;
-		let args = Array.from(arguments);
-		let stride = args.length;
-		if(TagPositionSprite.components == undefined){
-			TagPositionSprite.components = [];
-		}
-		for(let i = 0; i<stride;i++){
-			TagPositionSprite.components[id*stride + i] = args[i];
-		}
-	}
-
-	TagPositionSprite("Block",15,15,25,"block.png");
-	Position(0,0,0);
-	TagPositionSprite("Player",50,50,0,"player.png");
-	Position(15, 15, 0);
-	console.log(TagPositionSprite.components);
-	console.log(Position.components)
 }
+function LevelOne(){
+	let canvas = document.getElementById("canvas");
+	let context = canvas.getContext('2d');
 
+	let isocube = new Image();
+	isocube.src = "./Assets/isocube.png";
+
+	isocube.addEventListener("load",RenderIsocubes);
+}
 LevelOne();
